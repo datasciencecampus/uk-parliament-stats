50f32b4c1ba45f03175a3f46b81e297252fbea2e
Rory Corbett <54154358+rozzahh@users.noreply.github.com>
2021-04-22 17:23:39 +0100

    Implemented test classification model - predicting if speaker is from Con or other party

--- parliament-frequency-analysis.py
+++ parliament-frequency-analysis.py
@@ -18,7 +18,14 @@ Next steps:
 import pandas as pd
 import matplotlib.pyplot as plt
 import spacy
+import string
 from spacy.lang.en import English
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+from sklearn.base import TransformerMixin
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
 
 # --- Variables ---
@@ -115,43 +122,86 @@ mention_frequency.plot(x="week", y="size")
 # tokenize > stopword removal > lemmatization (root words) > 
 # other: part of speech (noun, adj, verb etc.), entity detection (could this be useful for identifying specific statistical outputs?), dependency parsing (may be important for sentiment analysis?), 
 
-#pull one comment containing ONS mention to help explore spacy functionality
-text = matchedrows.iloc[1,8]
-
 # Load English tokenizer, tagger, parser, NER and word vectors
 nlp = spacy.load('en_core_web_md')
 
-#  "nlp" Object is used to create documents with linguistic annotations.
-doc = nlp(text)
-
-
-# Create list of word tokens
-token_list = []
-for token in doc:
-    token_list.append(token.text)
-print(token_list)
-
-
-# create list of sentence tokens
-sents_list = []
-for sent in doc.sents:
-    sents_list.append(sent.text)
-print(sents_list)
-
 
 #stopwords
-spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+stopwords = spacy.lang.en.stop_words.STOP_WORDS
+# punctuation
+punctuation = string.punctuation
 
-# filtering stop words
-filtered_sent=[]
+# Creating our tokenizer function
+def spacy_tokenizer(sentence):
+    # Creating our token object, which is used to create documents with linguistic annotations.
+    mytokens = nlp(sentence)
 
-for word in doc:
-    if word.is_stop==False:
-        filtered_sent.append(word)
-print("Filtered Sentence:",filtered_sent)
+    # Lemmatizing each token and converting each token into lowercase
+    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
+
+    # Removing stop words
+    mytokens = [ word for word in mytokens if word not in stopwords and word not in punctuation ]
+
+    # return preprocessed list of tokens
+    return mytokens
+
+# Custom transformer using spaCy
+class predictors(TransformerMixin):
+    def transform(self, X, **transform_params):
+        # Cleaning Text
+        return [clean_text(text) for text in X]
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+
+# Basic function to clean the text
+def clean_text(text):
+    # Removing spaces and converting text into lowercase
+    return text.strip().lower()
+
+# - Create components for model
+
+#bag of words
+bow_vector = CountVectorizer(tokenizer = spacy_tokenizer, ngram_range=(1,1))
+
+#tfidf vectorization
+tfidf_vector = TfidfVectorizer(tokenizer = spacy_tokenizer)
+
+#classification method
+classifier = LogisticRegression()
+
+#dataset - group by party into 'Con' column - as Con (1) or Other (0)
+matchedrows.loc[matchedrows.party != 'Con', 'Con'] = 0
+matchedrows.loc[matchedrows.party == 'Con', 'Con'] = 1
 
 
-# finding lemma for each word
-for word in doc:
-    print(word.text,word.lemma_)
+# - Set up training/test data for model
+X = matchedrows['text'] # the features we want to analyze
+ylabels = matchedrows['Con'] # the labels, or answers, we want to test against
+
+X_train, X_test, y_train, y_test = train_test_split(X, ylabels, test_size=0.3)
+
+
+# - Create pipeline
+print("Creating pipeline...")
+pipe = Pipeline([("cleaner", predictors()),
+                 ('vectorizer', bow_vector),
+                 ('classifier', classifier)])
+
+
+# Run model with training datasets
+print("Training model...")
+pipe.fit(X_train,y_train)
+
+# Evaluate model with test datasets
+print("Testing model...")
+predicted = pipe.predict(X_test)
+
+# Model Accuracy
+print("Logistic Regression Accuracy:",metrics.accuracy_score(y_test, predicted))
+print("Logistic Regression Precision:",metrics.precision_score(y_test, predicted))
+print("Logistic Regression Recall:",metrics.recall_score(y_test, predicted))
     
