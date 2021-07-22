cf4089e5c8e08eadda619156c40d906d4ba34d7d
Rory Corbett <54154358+rozzahh@users.noreply.github.com>
2021-07-22 11:10:14 +0100

    updated to pull in major headings too

--- parlmentions/data_processing/xml_processing.py
+++ parlmentions/data_processing/xml_processing.py
@@ -18,6 +18,11 @@ root_node = ET.parse(xmlfile).getroot()
 # extract debate info (id & title)     
 debate_id = []
 debate_text = []
+for tag in root_node.findall('major-heading'):
+    debate_id_temp = tag.get('id')
+    debate_text_temp = tag.text
+    debate_id.append(debate_id_temp)
+    debate_text.append(debate_text_temp)
 for tag in root_node.findall('minor-heading'):
     debate_id_temp = tag.get('id')
     debate_text_temp = tag.text
@@ -62,14 +67,13 @@ for i in range(0,len(paragraph_id)):
 
 # create dataframes 
 df_debate = pd.DataFrame(data_debate, columns = ['debate_id','debate_text'])
-print(df_debate)
+df_debate.sort_values('debate_id', inplace=True)
 
 
 df_speech = pd.DataFrame(data_speech, columns = ['speech_id','speaker_name'])
 #extract string after end of date (e.g. in dev file - 'a.1677.3')
 df_speech["merge_id"] = df_speech["speech_id"].str.split('\d*-\d*-\d*', n=1)
 df_speech["merge_id"] = df_speech["merge_id"].str[1]
-print(df_speech)
 
 
 
@@ -78,8 +82,6 @@ df_paragraph = pd.DataFrame(data_paragraph, columns = ['paragraph_id','speech'])
 #extract string before '/'
 df_paragraph["merge_id"] = df_paragraph["paragraph_id"].str.split('/', n=1)
 df_paragraph["merge_id"] = df_paragraph["merge_id"].str[0]
-print(df_paragraph)
-
 
 #combine speech & paragraph dfs
 df = df_speech.merge(df_paragraph, on = "merge_id")
@@ -87,6 +89,7 @@ df = df_speech.merge(df_paragraph, on = "merge_id")
 #concat speech paragraphs together (so we have 1 row per speech)
 df.fillna("",inplace=True) #fill None in strings with blank
 df_merged_speech = df.groupby('merge_id')['speech'].agg(lambda col: '\n\n'.join(col)) #join speech rows together with 2 newlines
+
 #join merged speech back into dataset
 df_temp = df.drop(columns=['paragraph_id', 'speech']) #remove paragraph_id & speech columns
 df_temp = df_temp.drop_duplicates(subset=["merge_id"]) #drop duplicates on remaining rows - so we have 1 row per speech
@@ -95,6 +98,7 @@ df_full = df_temp.merge(df_merged_speech, on = "merge_id") #join in full speech
 
 #NEXT: 
     # need to bring in debate for each part of speech
+        # find nearest (smaller) ID number to get debate title that appeared before each speech
 
     
 
