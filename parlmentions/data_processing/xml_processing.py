d73fbee6f18029e871336d2c887bfe732e43d239
Rory Corbett <54154358+rozzahh@users.noreply.github.com>
2021-07-30 17:02:02 +0100

    developed approach for handling debate/speech numbering - applied for debate dataframe

--- parlmentions/data_processing/xml_processing.py
+++ parlmentions/data_processing/xml_processing.py
@@ -6,6 +6,7 @@ Created on Tue Jun 22 14:06:56 2021
 """
 
 import pandas as pd
+import numpy as np
 import xml.etree.ElementTree as ET
 
 
@@ -65,19 +66,35 @@ for i in range(0,len(paragraph_id)):
 ##need to extract date from filename and include this in df too  
    
 
-# create dataframes 
+# Create dataframes 
+
+
+## DEBATE
+
 df_debate = pd.DataFrame(data_debate, columns = ['debate_id','debate_text'])
 df_debate.sort_values('debate_id', inplace=True) #arrange major & minor headings in order in which they occurred
 #extract merge id as below for speech
 df_debate["merge_id"] = df_debate["debate_id"].str.split('\d*-\d*-\d*', n=1)
 df_debate["merge_id"] = df_debate["merge_id"].str[1]
-#extract numbers
-df_debate["merge_id"] = df_debate["merge_id"].str.extract(r'(\d+\D\d+)')
 
-#>>>
-#anything with 1 digit after the dp gets a left pad 0 (e.g. .3 -> .03) 
-# should then be ready to do merge_asof further down
-#>>>
+#sort out merge id numbers & convert to float
+df_debate["merge_id_check"] = df_debate['merge_id'].str[-2:]
+df_debate["merge_id_check"] = df_debate["merge_id_check"].str.replace("\\.", "", regex = True)
+
+df_debate['merge_id_test'] = np.where(df_debate['merge_id_check'].str.len() == 1, df_debate['merge_id_check'].str.pad(width=2, side='left', fillchar='0'), df_debate['merge_id_check']) #special characters need to be escaped '\\'
+
+df_debate["merge_id_split"] = df_debate["merge_id"].str.split('.')
+df_debate["testing"] = df_debate["merge_id_split"].str[:-1] #copy out all elements of list except the last
+
+df_debate["testing"] = df_debate["testing"].str.join('.')
+df_debate["testing"] = df_debate["testing"]+"."+df_debate["merge_id_test"]
+
+df_debate["merge_id"] = df_debate["testing"].str.extract(r'(\d+\D\d+)')
+df_debate["merge_id"] = df_debate["merge_id"].astype(float)
+
+
+
+## SPEECH
 
 df_speech = pd.DataFrame(data_speech, columns = ['speech_id','speaker_name'])
 #extract string after end of date (e.g. in dev file - 'a.1677.3')
@@ -85,12 +102,14 @@ df_speech["merge_id"] = df_speech["speech_id"].str.split('\d*-\d*-\d*', n=1)
 df_speech["merge_id"] = df_speech["merge_id"].str[1]
 
 
-
+## PARAGRAPH
 
 df_paragraph = pd.DataFrame(data_paragraph, columns = ['paragraph_id','speech'])
 #extract string before '/'
 df_paragraph["merge_id"] = df_paragraph["paragraph_id"].str.split('/', n=1)
 df_paragraph["merge_id"] = df_paragraph["merge_id"].str[0]
+
+
 
 #combine speech & paragraph dfs
 df = df_speech.merge(df_paragraph, on = "merge_id")
@@ -103,13 +122,27 @@ df_merged_speech = df.groupby('merge_id')['speech'].agg(lambda col: '\n\n'.join(
 df_temp = df.drop(columns=['paragraph_id', 'speech']) #remove paragraph_id & speech columns
 df_temp = df_temp.drop_duplicates(subset=["merge_id"]) #drop duplicates on remaining rows - so we have 1 row per speech
 df_full = df_temp.merge(df_merged_speech, on = "merge_id") #join in full speech 
+df_full["merge_id"] = df_full["merge_id"].str.extract(r'(\d+\D\d+)')
+df_full["merge_id"] = df_full["merge_id"].astype(float)
 
+
+
+
+# sort both dataframes by merge_id to facilitate merge
+df_debate.sort_values(by = "merge_id", inplace=True)
+df_full.sort_values(by = "merge_id", inplace=True)
 #merge debate titles with speech dataframe
-# pd.merge_asof(df_full, df_debate, on = ""
+df_complete = pd.merge_asof(df_full, df_debate, on = "merge_id")
+
 
 #NEXT: 
-    # need to bring in debate for each part of speech
-        # find nearest (smaller) ID number to get debate title that appeared before each speech
+    # tidy up col names in merge_id number section for debate
+    # apply merge_id number approach to others (write as function and apply?)
+    # need to extract date from filename and include this in df too  
+    # check final output
+    # tidy up final output (drop unnecessary vars, sort out var order, remove tab/whitespace from start of debate_text)
+    # test run on a few more files & do some QA
+    # modularise & set up process as outlined in comments below
 
     
 
